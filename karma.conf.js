64dc818dcff7653bf7a27b8df27683d008dfdb62
James Hollingworth <jamiehollingworth@gmail.com>
2015-05-25 16:34:36 +0100

    Use chrome

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeCanary'],
+    browsers: ['Chrome'],
     singleRun: false
   });
 };
